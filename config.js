1fd63767ade83eafe94979c8a067fe5f1fd7f919
Jake Obron <jbobron@gmail.com>
2014-12-24 11:34:55 -0800

    Built Ghost for production pt2

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://jbobron.azurewebsites.net/',
+        url: 'http://yourawesomewebsite.azurewebsites.net/',
         mail: {},
         database: {
             client: 'sqlite3',
